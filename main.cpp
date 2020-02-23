3076e61e29002516caab24c0acc0d21e91da8303
Joker2770 <1214220480@qq.com>
2020-02-23 13:24:23 +0800

    chore: CMake build :construction:

--- main.cpp
+++ main.cpp
@@ -1,11 +1,42 @@
-#include "mainwindow.h"
-#include <QApplication>
+// OS Specific sleep
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#include <string>
+#include <iostream>
+#include <cstdio>
+
+#include "serial/serial.h"
+
+using std::string;
+using std::exception;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::vector;
+using std::allocator;
+
+
+void enumerate_ports()
+{
+    vector<serial::PortInfo> devices_found = serial::list_ports();
+
+    vector<serial::PortInfo>::iterator iter = devices_found.begin();
+
+    while( iter != devices_found.end() )
+    {
+        serial::PortInfo device = *iter++;
+
+        printf( "(%s, %s, %s)\n", device.port.c_str(), device.description.c_str(), device.hardware_id.c_str() );
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    enumerate_ports();
 
-    return a.exec();
+	return 0;
 }
